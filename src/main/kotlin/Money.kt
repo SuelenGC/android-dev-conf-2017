5334dc9dd52475c95245ecd20460469a29be819c
David Robert <davidrobert@gmail.com>
2017-08-24 21:32:16 -0300

    Melhorias no exemplo

--- src/main/kotlin/Money.kt
+++ src/main/kotlin/Money.kt
@@ -3,7 +3,14 @@ package main.kotlin
 import main.java.JavaMoney
 import java.math.BigDecimal
 
-data class Money(val amount: BigDecimal, val currency: String)
+data class Money(val amount: BigDecimal, val currency: String) {
+    operator fun plus(money: Money) =
+            if (currency == money.currency) {
+                Money(amount + money.amount, currency)
+            } else {
+                throw IllegalArgumentException("Bla bla bla")
+            }
+}
 
 fun sendPayment(money: Money, message: String = "") {
     println("Sending ${money.amount} - ${message}")
@@ -14,7 +21,8 @@ fun sum(x: Int, y: Int) = x + y
 fun convertToDollars(money: Money): Money {
     when (money.currency) {
         "U$" -> return money
-        "R$" -> return Money(money.amount * BigDecimal(3.50), "U$")        else -> throw IllegalArgumentException("Ixi deu ruim!")
+        "R$" -> return Money(money.amount * BigDecimal(3.50), "U$")
+        else -> throw IllegalArgumentException("Ixi deu ruim!")
     }
 }
 
@@ -27,22 +35,55 @@ fun convertToDollars2(money: Money): Money {
 }
 
 fun convertToDollars3(money: Money) = when (money.currency) {
-    "$" -> money
+    "U$" -> money
     "R$" -> Money(money.amount / BigDecimal(3.50), "U$")
     else -> throw IllegalArgumentException("Ixi deu ruim!")
 }
 
 fun BigDecimal.percent(percentage: Int) = this.multiply(java.math.BigDecimal(percentage)).divide(BigDecimal(100))
+
+infix fun Int.percentOf(money: Money) = money.amount.multiply(BigDecimal(this)).divide(BigDecimal(100));
+
+
+fun javaMoney(money: JavaMoney?) {
+    if (money != null) {
+        println("${money.amount} <<")
+    }
+
+    // ou
+
+    println("${money?.amount} <<<<")
+
+    // ou se quiser dar um tiro no pé
+    // println("${money!!.amount} <<<<")
+}
+
 fun main(args: Array<String>) {
 
-    val tickets = Money(BigDecimal(100), "R$")
-    val popcorn = tickets.copy(BigDecimal(25), "R$")
+    val tickets = Money(100.bd, "R$")
+    val popcorn = tickets.copy(25.bd, "R$")
 
-    val bd = BigDecimal(100)
-    // bd.percent ???
+    val costs = tickets + popcorn
+    println(costs)
 
-    println(bd.percent(7))
 
+    val x : Money = Money(100.bd, "U$")
+    // x = null
+
+    val big = BigDecimal(100)
+    // big.percent ???
+
+    println(big.percent(7))
+
+    println(50.percentOf(popcorn))
+
+    // Para corrigir o erro que vai aparecer:
+    // Alt + Enter
+    // Add infix
+    println(50 percentOf popcorn)
+
+    val long = 100L
+    val bd = 100.bd
 
     if (tickets != popcorn) {
         println("They are differents!")
@@ -58,4 +99,10 @@ fun main(args: Array<String>) {
 
     println(javaMoney.amount)
 
+    main.kotlin.javaMoney(javaMoney)
+    main.kotlin.javaMoney(null)
 }
+
+private val Int.bd: BigDecimal
+    get() = BigDecimal(this)
+
